c522e6f4b1775ee2e06c0ab53b92fc62004ed300
swachchhandshrestha <swachchhandshrestha11@gmail.com>
2020-07-07 23:24:52 +0545

    working with custom drawers

--- screens/DrawerContent.js
+++ screens/DrawerContent.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { 
          View, 
-         StyleSheet 
+         StyleSheet
         } from 'react-native';
 import {
     Avatar,
@@ -19,10 +19,90 @@ import {
     DrawerItem
 } from '@react-navigation/drawer';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
 export function DrawerContent(props){
     return(
-        <View>
-            <Text>this is a test</Text>
+        <View style={{flex:1}}>
+            <DrawerContentScrollView {...props}>
+                <View style={styles.drawerContent}>
+                    <View style={styles.userInfoSection}>
+                        <View>
+                            <Avatar.Image
+                                source={{
+                                    uri: 'https://api.adorable.io/avatars/50/abott@adorable.png'
+                                    
+                                }}
+                                size= {50}
+                            />
+                                <View>
+                                    <Title style={styles.title}>Swachchhand Shrestha</Title>
+                                    <Caption style={styles.caption}>@Swa-Shrestha</Caption>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </DrawerContentScrollView>
+            <Drawer.Section style={styles.bottomDrawerSection}>
+                <DrawerItem
+                    icon={({color, size}) => (
+                        <Icon
+                            name="exit-to-app"
+                            color={color}
+                            size={size}
+                        />
+                        )}
+                        label="Sign Out"
+                        onPress={() => {}}
+                    />
+
+            </Drawer.Section>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    drawerContent: {
+      flex: 1,
+    },
+    userInfoSection: {
+      paddingLeft: 20,
+    },
+    title: {
+      fontSize: 16,
+      marginTop: 3,
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: 14,
+      lineHeight: 14,
+    },
+    row: {
+      marginTop: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    section: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginRight: 15,
+    },
+    paragraph: {
+      fontWeight: 'bold',
+      marginRight: 3,
+    },
+    drawerSection: {
+      marginTop: 15,
+    },
+    bottomDrawerSection: {
+        marginBottom: 15,
+        borderTopColor: '#f4f4f4',
+        borderTopWidth: 1
+    },
+    preference: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+    },
+  });
